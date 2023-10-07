9e8aed02d8c5f2057ed282323e5401d54034af55
Kleybrink <Kleybrink@users.noreply.github.com>
2023-10-07 18:52:19 +0200

    feat: Enhance pyscriptaggregator and update .gitignore
    1. `.gitignore` updates:
       - Added .vscode to the ignore list.
    
    2. Improvements in `pyscriptaggregator/sagg.py`:
       - Reorganized imports for clarity.
       - Removed unused `prev_toktype` in `remove_comments` function.
       - Introduced `extract_definitions` function to extract Python class, function, and variable definitions.
       - Adjusted command line arguments:
         - Shortened version for `--remove-comments` (-r).
         - Renamed `--output` to `--output-file` with a shortened version (-o).
         - Renamed `--depth` to `--search-depth` with a shortened version (-d).
         - Added `--show-defs` (-s) to display only definitions.
         - Added `--no-filter-for` (-e) to specify filenames to exclude from filtering.
       - Enhanced the `main` function to handle the new `--show-defs` and `--no-filter-for` arguments.
       - Adjusted the output for token count and written files.

--- pyscriptaggregator/sagg.py
+++ pyscriptaggregator/sagg.py
@@ -1,8 +1,10 @@
-import os
 import argparse
+import ast
 import logging
-from io import StringIO
+import os
 import tokenize
+from io import StringIO
+
 import tiktoken
 
 
@@ -47,42 +49,112 @@ def remove_comments(source):
     return result.getvalue()
 
 
+def extract_definitions(source_code):
+    try:
+        root = ast.parse(source_code)
+    except SyntaxError as e:
+        return f"Error parsing source code: {e}"
+
+    classes = []
+    functions = []
+    variables = []
+
+    for node in root.body:  # nur direkte Kinder des AST-Roots
+        try:
+            if isinstance(node, ast.FunctionDef) and not node.name.startswith("_"):
+                func_name = node.name
+                arguments = [arg.arg for arg in node.args.args]
+                functions.append(f"{func_name}({', '.join(arguments)})")
+            elif isinstance(node, ast.ClassDef) and not node.name.startswith("_"):
+                init_args = []
+                for child in node.body:
+                    if isinstance(child, ast.FunctionDef) and child.name == "__init__":
+                        init_args = [
+                            arg.arg for arg in child.args.args if arg.arg != "self"
+                        ]
+                class_repr = node.name
+                if init_args:
+                    class_repr += f"({', '.join(init_args)})"
+                classes.append(class_repr)
+            elif isinstance(node, ast.Assign):
+                for target in node.targets:
+                    if isinstance(target, ast.Name) and not target.id.startswith("_"):
+                        variables.append(target.id)
+        except Exception as e:
+            return f"Error processing node {node}: {e}"
+
+    output = []
+
+    if classes:
+        output.append("Classes:")
+        for c in classes:
+            output.append(f"  {c}")
+
+    if functions:
+        output.append("Functions:")
+        for f in functions:
+            output.append(f"  {f}")
+
+    if variables:
+        output.append("Variables:")
+        for v in variables:
+            output.append(f"  {v}")
+
+    return "\n".join(output)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="Combine .py files from current directory and subdirectories."
     )
+
     parser.add_argument(
+        "-r",
         "--remove-comments",
         action="store_true",
         help="Remove comments from the .py files.",
     )
     parser.add_argument(
-        "--output",
+        "-o",
+        "--output-file",
         default="combined_code.txt",
         help="Specify the filename for the output file.",
     )
     parser.add_argument(
-        "--depth",
+        "-d",
+        "--search-depth",
         type=int,
         default=1,
         help="Maximum directory depth to search. Default is 1, which means only the next level of folders.",
     )
+    parser.add_argument(
+        "-s",
+        "--show-defs",
+        action="store_true",
+        help="Show only definitions instead of full file content.",
+    )
+    parser.add_argument(
+        "-e",
+        "--no-filter-for",
+        default=None,
+        help="Specify the filename for which you want to show the full content, excluding it from other filters.",
+    )
 
     args = parser.parse_args()
 
-    if os.path.exists(args.output):
+    if os.path.exists(args.output_file):
         logging.warning(
-            f"Output file {args.output} already exists. It will be overwritten."
+            f"Output file {args.output_file} already exists. It will be overwritten."
         )
 
     current_directory = os.getcwd()
     start_depth = current_directory.count(os.path.sep)
     included_files = []
 
-    with open(args.output, "w") as output_file:
+    with open(args.output_file, "w") as output_file:
         for dirpath, dirnames, filenames in os.walk(current_directory):
             current_depth = dirpath.count(os.path.sep) - start_depth
-            if current_depth > args.depth:
+            if current_depth > args.search_depth:
                 del dirnames[:]
                 continue
 
@@ -99,8 +171,13 @@ def main():
                             os.path.join(dirpath, filename), "r", encoding="utf-8"
                         ) as file:
                             content = file.read()
-                            if args.remove_comments:
-                                content = remove_comments(StringIO(content))
+                            if args.no_filter_for and filepath == args.no_filter_for:
+                                pass
+                            else:
+                                if args.remove_comments:
+                                    content = remove_comments(StringIO(content))
+                                if args.show_defs:
+                                    content = extract_definitions(content)
                             output_file.write(f"===== {filepath} =====\n")
                             output_file.write(content + "\n\n")
                             included_files.append(filepath)
@@ -108,7 +185,7 @@ def main():
                         logging.error(f"Failed to process {filepath}. Error: {e}")
 
     enc = tiktoken.encoding_for_model("gpt-4")
-    with open(args.output, "r", encoding="utf-8") as f:
+    with open(args.output_file, "r", encoding="utf-8") as f:
         content = f.read()
         token_count = len(enc.encode(content))
 
@@ -116,8 +193,8 @@ def main():
     print(f"Included Files: {len(included_files)}")
     for file in included_files:
         print(f"  - {file}")
-    print(f"Written to: {args.output}")
-    print(f"Total Tokens in {args.output}: {token_count}")
+    print(f"Written to: {args.output_file}")
+    print(f"Total Tokens in {args.output_file}: {token_count}")
 
 
 if __name__ == "__main__":
